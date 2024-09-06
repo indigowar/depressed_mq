8e54cae76bf13773750cd33a4d6d27cc918eb555
indigowar <a10levsion@gmail.com>
2024-09-07 00:35:04 +0300

    reimplemented indices with bincode

--- src/storage/offset_index.rs
+++ src/storage/offset_index.rs
@@ -1,30 +1,15 @@
 use std::{
-    fs::{File, OpenOptions},
+    cell::RefCell,
+    fs::{self, File, OpenOptions},
     io::{Error, ErrorKind, Read, Seek, SeekFrom, Write},
+    os::unix::fs::MetadataExt,
 };
 
 use serde::{Deserialize, Serialize};
 
-// OffsetIndex used to manage XXXXXX.index file for XXXXXX partition.
-// It stores `logical offset` to `physical byte offset`.
 pub struct OffsetIndex {
-    file: File,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Index {
-    logical: usize,
-    physical: usize,
-}
-
-impl Index {
-    pub fn size_serialized() -> usize {
-        let index = Index {
-            logical: 0,
-            physical: 0,
-        };
-        serde_cbor::to_vec(&index).unwrap().len()
-    }
+    file: RefCell<File>,
+    path: String,
 }
 
 impl OffsetIndex {
@@ -33,46 +18,100 @@ impl OffsetIndex {
             .create(true)
             .read(true)
             .write(true)
-            .open(path)
+            .open(path.clone())
         {
-            Ok(file) => Ok(Self { file }),
+            Ok(file) => Ok(Self {
+                file: RefCell::new(file),
+                path,
+            }),
             Err(e) => Err(e),
         }
     }
 
-    pub fn read(&mut self, logical: usize) -> Result<usize, Error> {
-        let index_size = Index::size_serialized();
-        let mut buffer = Vec::with_capacity(index_size);
+    pub fn write(&mut self, logical: usize, physical: usize) -> Result<(), Error> {
+        let data = Index::serialize(Index { logical, physical })?;
 
-        self.file.seek(SeekFrom::Start(0))?;
+        let mut file = self.file.borrow_mut();
+
+        file.seek(SeekFrom::End(0))?;
+        file.write_all(&data)
+    }
+
+    pub fn read(&self, logical: usize) -> Result<usize, Error> {
+        let mut buffer = vec![0; Index::size()];
+
+        self.file.borrow_mut().seek(SeekFrom::Start(0))?;
         loop {
-            let bytes_read = self.file.read(&mut buffer)?;
-            if bytes_read != index_size {
-                return Err(Error::new(
-                    ErrorKind::NotFound,
-                    "index not found in the file",
-                ));
-            }
-
-            let index: Index =
-                serde_cbor::from_slice(&buffer).map_err(Self::serialization_error)?;
-
+            let index = self.read_index(Some(&mut buffer))?;
             if index.logical == logical {
                 return Ok(index.physical);
             }
         }
     }
 
-    pub fn write(&mut self, logical: usize, physical: usize) -> Result<(), Error> {
-        let index = Index { logical, physical };
-        let encoded = serde_cbor::to_vec(&index).map_err(Self::serialization_error)?;
+    pub fn latest_logical(&self) -> Result<usize, Error> {
+        let offset = -(Index::size() as i64);
 
-        self.file.seek(SeekFrom::End(0))?;
+        self.file.borrow_mut().seek(SeekFrom::End(offset))?;
+        let index = self.read_index(None)?;
 
-        self.file.write_all(&encoded)
+        Ok(index.logical)
     }
 
-    fn serialization_error(e: serde_cbor::Error) -> Error {
+    pub fn is_empty(&self) -> Result<bool, Error> {
+        Ok(fs::metadata(self.path.clone())?.size() == 0)
+    }
+
+    pub fn is_full(&self, size: usize) -> Result<bool, Error> {
+        let m = fs::metadata(self.path.clone())?;
+        let size = size * Index::size();
+        Ok(m.size() == size as u64)
+    }
+
+    pub fn read_index(&self, buffer: Option<&mut [u8]>) -> Result<Index, Error> {
+        let mut owned_buffer;
+        let buffer = match buffer {
+            Some(buf) => buf,
+            None => {
+                owned_buffer = vec![0u8; Index::size()];
+                owned_buffer.as_mut_slice()
+            }
+        };
+
+        let bytes_read = self.file.borrow_mut().read(buffer)?;
+        if bytes_read != Index::size() {
+            return Err(Error::new(
+                ErrorKind::NotFound,
+                "index not found in the file",
+            ));
+        }
+
+        Index::deserialize(buffer)
+    }
+}
+
+#[derive(Serialize, Deserialize, Default)]
+struct Index {
+    logical: usize,
+    physical: usize,
+}
+
+impl Index {
+    pub fn size() -> usize {
+        let index = Index::default();
+
+        bincode::serialize(&index).unwrap().len()
+    }
+
+    pub fn deserialize(buffer: &[u8]) -> Result<Self, Error> {
+        bincode::deserialize(buffer).map_err(Self::error)
+    }
+
+    pub fn serialize(index: Index) -> Result<Vec<u8>, Error> {
+        bincode::serialize(&index).map_err(Self::error)
+    }
+
+    fn error(e: bincode::Error) -> Error {
         Error::new(ErrorKind::InvalidData, e)
     }
 }
--- src/storage/timestamp_index.rs
+++ src/storage/timestamp_index.rs
@@ -1,4 +1,5 @@
 use std::{
+    cell::RefCell,
     fs::{File, OpenOptions},
     io::{Error, ErrorKind, Read, Seek, SeekFrom, Write},
 };
@@ -6,26 +7,9 @@ use std::{
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-// TimestampIndex used to manage XXXXXXXXx.timestamp file for XXXXXXXXx partition.
-// It stores `timestamp` to `logical offset`.
 pub struct TimestampIndex {
-    file: File,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Index {
-    offset: usize,
-    timestamp: i64,
-}
-
-impl Index {
-    pub fn size_serialized() -> usize {
-        let index = Index {
-            offset: 0,
-            timestamp: 0,
-        };
-        serde_cbor::to_vec(&index).unwrap().len()
-    }
+    file: RefCell<File>,
+    path: String,
 }
 
 impl TimestampIndex {
@@ -34,53 +18,90 @@ impl TimestampIndex {
             .create(true)
             .read(true)
             .write(true)
-            .open(path)
+            .open(path.clone())
         {
-            Ok(file) => Ok(Self { file }),
+            Ok(file) => Ok(Self {
+                file: RefCell::new(file),
+                path,
+            }),
             Err(e) => Err(e),
         }
     }
 
-    pub fn read(&mut self, timestamp: DateTime<Utc>) -> Result<usize, Error> {
-        let timestamp = timestamp.timestamp_nanos_opt().unwrap();
-        let index_size = Index::size_serialized();
+    pub fn read(&self, _timestamp: DateTime<Utc>) -> Result<usize, Error> {
+        let timestamp = _timestamp.timestamp_nanos_opt().unwrap();
 
-        let mut buffer = Vec::with_capacity(index_size);
+        let mut buffer = vec![0u8; Index::size()];
 
-        self.file.seek(SeekFrom::Start(0))?;
-
+        self.file.borrow_mut().seek(SeekFrom::Start(0))?;
         loop {
-            let bytes_read = self.file.read(&mut buffer)?;
-            if bytes_read != index_size {
-                return Err(Error::new(
-                    ErrorKind::NotFound,
-                    "index not found in the file",
-                ));
-            }
-
-            let index: Index =
-                serde_cbor::from_slice(&buffer).map_err(Self::serialization_error)?;
-
+            let index = self.read_index(Some(&mut buffer))?;
             if index.timestamp == timestamp {
                 return Ok(index.offset);
             }
         }
     }
 
-    pub fn write(&mut self, timestamp: DateTime<Utc>, offset: usize) -> Result<(), Error> {
-        let index = Index {
-            offset,
-            timestamp: timestamp.timestamp_nanos_opt().unwrap(),
-        };
+    pub fn write(&self, timestamp: DateTime<Utc>, offset: usize) -> Result<(), Error> {
+        let data = Index::serialize(Index::new(timestamp, offset))?;
 
-        let encoded = serde_cbor::to_vec(&index).map_err(Self::serialization_error)?;
+        let mut file = self.file.borrow_mut();
 
-        self.file.seek(SeekFrom::End(0))?;
-
-        self.file.write_all(&encoded)
+        file.seek(SeekFrom::End(0))?;
+        file.write_all(&data)
     }
 
-    fn serialization_error(e: serde_cbor::Error) -> Error {
+    fn read_index(&self, buffer: Option<&mut [u8]>) -> Result<Index, Error> {
+        let mut owned_buffer;
+        let buffer = match buffer {
+            Some(buf) => buf,
+            None => {
+                owned_buffer = vec![0u8; Index::size()];
+                owned_buffer.as_mut_slice()
+            }
+        };
+
+        let bytes_read = self.file.borrow_mut().read(buffer)?;
+        if bytes_read != Index::size() {
+            return Err(Error::new(
+                ErrorKind::NotFound,
+                "index not found in the file",
+            ));
+        }
+
+        Index::deserialize(buffer)
+    }
+}
+
+#[derive(Serialize, Deserialize, Default)]
+struct Index {
+    offset: usize,
+    timestamp: i64,
+}
+
+impl Index {
+    pub fn new(ts: DateTime<Utc>, offset: usize) -> Index {
+        Index {
+            offset,
+            timestamp: ts.timestamp_nanos_opt().unwrap(),
+        }
+    }
+
+    pub fn size() -> usize {
+        let index = Index::default();
+
+        bincode::serialize(&index).unwrap().len()
+    }
+
+    pub fn deserialize(buffer: &[u8]) -> Result<Self, Error> {
+        bincode::deserialize(buffer).map_err(Self::error)
+    }
+
+    pub fn serialize(index: Index) -> Result<Vec<u8>, Error> {
+        bincode::serialize(&index).map_err(Self::error)
+    }
+
+    fn error(e: bincode::Error) -> Error {
         Error::new(ErrorKind::InvalidData, e)
     }
 }
